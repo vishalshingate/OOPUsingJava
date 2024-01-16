07dd55df7b4b0fa087891d898c1652dd6fa927f9
vishal shingate <vshingate@ptc.com>
2024-01-16 17:51:27 +0530

    use of static code

--- com/pluralsight/javaoopFundametals/Demo.java
+++ com/pluralsight/javaoopFundametals/Demo.java
@@ -1,15 +1,19 @@
 package com.pluralsight.javaoopFundametals;
 
+import javax.xml.catalog.Catalog;
+
 public class Demo {
     public static void main(String[] args) {
         ShoppingCart cart = new ShoppingCart();
-        Product toothBrush = new Product("ToothBrush",1200);
-        Product babyNapkin = new Product("BabyNapkin", 300);
+       /* Product toothBrush = new Product("ToothBrush",1200);
+        Product babyNapkin = new Product("BabyNapkin", 300);*/
 
+        Product toothBrush = Catalogue.getProduct("Electric ToothBrush");
+        Product babyAlarm = Catalogue.getProduct("Baby Alarm");
         cart.addProduct(toothBrush);
-        cart.addProduct(babyNapkin);
+        cart.addProduct(babyAlarm);
         System.out.println(cart);
-        System.out.println("total cost of the cart:-"+ cart.getTotalCost());
+        System.out.println("total cost of the cart: "+ cart.getTotalCost());
 
     }
 }
